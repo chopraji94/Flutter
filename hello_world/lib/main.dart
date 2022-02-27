9c707a8866e00e75dc63e2cddd8698565f14f226
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2022-02-27 20:50:33 +0530

    Calculator_App

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,108 +1,20 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'My App',
-      home: new LoginPage(),
-      theme: new ThemeData(primarySwatch: Colors.blue),
+    return MaterialApp(
+      title: 'Calculator App',
+      home: new HomePage(),
+      theme: new ThemeData(
+        primarySwatch: Colors.red
+      ),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
-  late AnimationController _iconAnimationController;
-  late Animation<double> _iconAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _iconAnimationController = new AnimationController(
-        vsync: this, duration: new Duration(milliseconds: 500));
-    _iconAnimation = new CurvedAnimation(
-      parent: _iconAnimationController,
-      curve: Curves.bounceOut,
-    );
-    _iconAnimation.addListener(() => this.setState(() {}));
-    _iconAnimationController.forward();
-  }
-
-  Widget AppBody() {
-    return new Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        new Image(
-          image: new AssetImage("assets/Background.jpg"),
-          fit: BoxFit.cover,
-          color: Colors.black87,
-          colorBlendMode: BlendMode.darken,
-        ),
-        new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new FlutterLogo(
-              size: _iconAnimation.value * 100,
-            ),
-            new Form(
-              child: Theme(
-                data: new ThemeData(
-                    brightness: Brightness.dark,
-                    primarySwatch: Colors.teal,
-                    inputDecorationTheme: new InputDecorationTheme(
-                        labelStyle:
-                            TextStyle(color: Colors.teal, fontSize: 20.0))),
-                child: Container(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration:
-                            new InputDecoration(labelText: "Enter email"),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      new TextFormField(
-                        decoration:
-                            new InputDecoration(labelText: "Enter Password"),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      new Padding(padding: const EdgeInsets.only(top: 20.0)),
-                      new MaterialButton(
-                          height: 40.0,
-                          minWidth: 100.0,
-                          onPressed: () => {},
-                          color: Colors.teal,
-                          splashColor: Colors.red,
-                          textColor: Colors.white,
-                          child: new Icon(Icons.login)
-                        ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.black,
-      body: AppBody(),
-    );
-  }
-}
