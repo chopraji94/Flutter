ff3c5f0397098f28657e7b4cf2c3a7e5efdf12a9
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2022-03-19 16:39:30 +0530

    Completed json fetch app

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,51 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() => runApp(new MaterialApp(
-    home: new HomePage(),
-    theme: new ThemeData(
-        primarySwatch: Colors.teal, brightness: Brightness.light)));
+      home: new MyHomePage(),
+      theme: new ThemeData(
+          primarySwatch: Colors.teal, brightness: Brightness.light),
+    ));
 
-class HomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MyHomePageState extends State<MyHomePage> {
+  final String url = "https://swapi.dev/api/people";
   late List data;
 
   @override
+  void initState() {
+    super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async {
+    var response = await
+        http.get(Uri.parse(url), headers: {"Accept": "application/json"});
+      print(response.body);
+
+    setState(() {
+      var convertData = json.decode(response.body);
+      data = convertData['results'];
+    });
+    return "Success";
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: new AppBar(
-        title: Text("Load Json App"),
+        title: Text('Json Get By Fetch'),
       ),
       body: new Container(
-        child: new Center(
-          child: new FutureBuilder(
-            future: DefaultAssetBundle.of(context)
-                .loadString('load_json/person.json'),
-            builder: (context, snapshot) {
-              var data = json.decode(snapshot.data.toString());
-
-              return new ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return new Card(
-                    child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        new Text("Name : " + data[index]["name"]),
-                        new Text("Age : " + data[index]["age"]),
-                        new Text("Gender : " + data[index]["gender"]),
-                        new Text("Hair Color : " + data[index]["hair_color"]),
-                      ],
+        child: new ListView.builder(
+          itemBuilder: (context, int index) {
+            return new Container(
+              padding: const EdgeInsets.all(8.0),
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new Card(
+                    child: new Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: new Text(data[index]['name']),
                     ),
-                  );
-                },
-                itemCount: data == null ? 0 : data.length,
-              );
-            },
-          ),
+                  ),
+                ],
+              ),
+            );
+          },
+          itemCount: data.length,
         ),
       ),
     );
