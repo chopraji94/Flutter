914d2fabdf68a0eebec7f1cfaee3b32dc5396030
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2022-03-26 22:16:38 +0530

    Completed drawerer and navigation

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,39 +1,74 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:hello_world/home_page.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:hello_world/new_page.dart';
 
-void main() async {
-  List currencies = await getCurrencies();
-  print(currencies);
-  runApp(new MyApp(currencies));
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  final List _currencies;
-  MyApp(this._currencies);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: new MyHomePage(_currencies),
+    return new MaterialApp(
       theme: new ThemeData(
-          primarySwatch: Colors.pink, 
-          brightness: Brightness.light,
-          primaryColor: defaultTargetPlatform == TargetPlatform.iOS ? Colors.grey[100] : null
-        ),
+          primarySwatch: Colors.amber, brightness: Brightness.light),
+      home: new HomePage(),
+      // routes: <String, WidgetBuilder>{
+      //   "/a": (BuildContext context) => new NewPage("New Page"),
+      // },
     );
   }
 }
 
-Future<List> getCurrencies() async {
-  String cryptoUrl =
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
-
-  http.Response response = await http.get(Uri.parse(cryptoUrl),
-      headers: {'X-CMC_PRO_API_KEY': '387e7cc1-01a8-4463-bb5e-26d9cf6b29ed'});
-  var jsonData = json.decode(response.body);
-  var onlyData = jsonData["data"] as List;
-  return onlyData;
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Drawer App'),
+        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Text('Home Page'),
+        ),
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('Pranav Chopra'),
+              accountEmail: new Text('Chopraji94@gmail.com'),
+              currentAccountPicture: new CircleAvatar(
+                  backgroundColor: Colors.black, child: new Text('P')),
+              otherAccountsPictures: <Widget>[
+                new CircleAvatar(
+                    backgroundColor: Colors.black, child: new Text('C')),
+              ],
+            ),
+            new ListTile(
+                title: new Text('Page One'),
+                trailing: new Icon(Icons.arrow_upward),
+                onTap: () => {
+                  Navigator.of(context).pop(),
+                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new NewPage('Page One')))
+                },
+            ),
+            new ListTile(
+              title: new Text('Page two'),
+              trailing: new Icon(Icons.arrow_upward),
+              onTap: () => {
+                Navigator.of(context).pop(),
+                Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new NewPage('Page two')))
+              },
+            ),
+            new Divider(height: 10.0),
+            new ListTile(
+              title: new Text('Close'),
+              trailing: new Icon(Icons.close),
+              onTap: () => Navigator.of(context).pop(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
